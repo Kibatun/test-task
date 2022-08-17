78f14fce3a0ab46410220795c3dabdedf84743c5
Семён <90569031+Kibatun@users.noreply.github.com>
2022-08-17 09:32:41 +0500

    Update 1.cs

--- 1.cs
+++ 1.cs
@@ -6,31 +6,28 @@ public static class Globals
     //где х изменяется от 0 до 360 градусов с шагом в 20 градусов.
     // Значения x в радианах выводить с точностью до 3 знаков после запятой, значения функций – до 4.
     //
-
-    internal static void Main()
+       internal static void Main()
     {
-        int i;
-        int x;
-        double xr;
-        double y1;
-        double y2;
-        double y3;
-        double pi = 3.14159265F;
-        Console.Write("+-----------------------------------+\n");
-        Console.Write("x,grad|x,rad| y1(x) | y2(x) | y3(x) |\n");
-        Console.Write("+-----------------------------------+\n");
-        for (i = 0; i <= 18; i++)
+        int cons;
+        Console.WriteLine("+-----------------------------------+");
+        Console.WriteLine("x,grad|x,rad| y1(x) | y2(x) | y3(x) |");
+        Console.WriteLine("+-----------------------------------+");
+        for (cons = 0; cons <= 18; cons++)
         {
-            x = i * 20;
-            xr = x * pi / 180;
-            y1 = Math.Sin(xr) + (Math.Sin(3 * xr) / 3);
-            y2 = y1 + (Math.Sin(5 * xr) / 5);
-            y3 = y2 + (Math.Sin(7 * xr) / 7);
-            Console.Write("|{0,3:D}  |{1,5:f3}|{2,7:f4}|{3,7:f4}|{4,7:f4}|\n", x, xr, y1, y2, y3);
+            int grad;
+            double rad;
+            double y1;
+            double y2;
+            double y3;
+            double pi = 3.14159265F;
+            grad = cons * 20;
+            rad = grad * pi / 180;
+            y1 = Math.Sin(rad) + (Math.Sin(3 * rad) / 3);
+            y2 = y1 + (Math.Sin(5 * rad) / 5);
+            y3 = y2 + (Math.Sin(7 * rad) / 7);
+            Console.WriteLine("|{0,3:D}  |{1,5:f3}|{2,7:f4}|{3,7:f4}|{4,7:f4}|", grad, rad, y1, y2, y3);
         }
-        Console.Write("+-----------------------------------+\n");
+        Console.WriteLine("+-----------------------------------+");
         Console.ReadKey(true);
     }
-
-
 }
