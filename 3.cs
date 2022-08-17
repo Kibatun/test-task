2d90faa97e0b25560b9e43afd95d5a64aeca893c
Семён <90569031+Kibatun@users.noreply.github.com>
2022-08-17 09:45:39 +0500

    Update 3.cs

--- 3.cs
+++ 3.cs
@@ -5,22 +5,21 @@ public static class Globals
     //В массиве X=(x1,x2,…,xn) определить минимальный и максимальный элементы. Не упорядочивая массив, удалить из него элементы,
     //расположенные между максимальным и минимальным. 
     //
-     internal static void Main()
+    internal static void Main()
     {
         int[] arr = new int[100];
-        int amount;
-        int dig;
-        int max;
-        int min;
-        int nmax;
-        int nmin;
-        Console.Write("Введите количество элементов\n");
+        int amount;        
+        Console.WriteLine("Введите количество элементов");
         int numb = int.Parse(Console.ReadLine());
         Console.WriteLine("Введите массив X");
         for (amount = 0; amount < numb; amount++)
         {
             arr[amount] = int.Parse(Console.ReadLine());
         }
+        int max;
+        int min;
+        int nmax;
+        int nmin;
         max = arr[0];
         nmax = 0;
         min = arr[0];
@@ -43,7 +42,7 @@ public static class Globals
                 nmin = amount;
             }
         }
-
+        int dig;
         for (dig = 1; dig <= nmax - nmin - 1; dig++)
         {
             for (amount = nmin + 1; amount <= numb - 2; amount++)
